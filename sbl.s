2fd3dd81b59828507b65a1940f1416d8ed07b14d
Alexander Surma <surma@petrelli.localdomain>
2009-11-28 20:22:14 +0100

    First disk read

--- sbl.s
+++ sbl.s
@@ -1,3 +1,4 @@
+init:
 	cli			; Disable interrupts
 
 	mov	ax,0x7c0	; We are at 0000:07c0
@@ -10,20 +11,45 @@
 	mov	sp,0
 	sti			; Enable interrupts
 
-	mov	si, wel		; Print welcome
+	mov	si, welcome_msg	; Print welcome
 	call	print_string
 
-	mov	al,0xFE
+	mov	ah,0x00		; Reset Disks
+	int	0x13
+
+read_disk:
+	mov	ah,0x02		; Read disk
+	mov	al,1		; read 1 Sector at:
+	mov	ch,0		; Track 0
+	mov	cl,1		; Sector 1
+	mov	dh,0		; Head 0
+	mov	bx,512		; Write to ES:BX
+	int	0x13
+
+	jb	.error
+	mov	si,disk_read_success
+	jmp	.print
+.error:
+	mov	si,disk_read_fail
+.print:
+	call	print_string
+
+	mov	si, 512
+debug:
+.loop:
+	lodsb
 	call	print_hex
-
-	jmp	$
-
+	mov	cx, si
+	cmp	cx, 512+512
+	je	.end
+	jmp	.loop
+.end:
 
 
 
 ;;;;;;;;;;
 ; Prints characters until 0-byte
-; CS:SI = start of string
+; ES:SI = start of string
 ;;;;;;;;;;
 
 print_string:
@@ -65,7 +91,9 @@ print_hex:
 .end:
 	ret
 
-wel:	db	"=== Suckless Bootloader ===",0xA,0xD,0
+welcome_msg	db	"=== Suckless Bootloader ===",0xA,0xD,0
+disk_read_success	db "[*] Disk read successfull",0xA,0xD,0
+disk_read_fail		db "[!] Disk read failed!",0xA,0xD,0
 
 times 510-($-$$) db 0		; MBR Signature
 dw 0xAA55
